1b89a30d542cee0629f8333269c8d0f937c01b19
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2024-11-15 07:43:02 +0700

    feat(ci-cd): setup ci-cd

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 ## 0.0.1
 
-* TODO: Describe initial release.
+* Support platform google cloud logging
+* Support platform betterstack logging
--- test/flutter_feature_remote_logging_test.dart
+++ test/flutter_feature_remote_logging_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_feature_remote_logging/flutter_feature_remote_logging.dart';
-
 void main() {
   test('adds one to input values', () {});
 }
